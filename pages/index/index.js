010dc2dd802fecbfd86f288b593a466850f8c7b4
johnnyzhang <1113638674@qq.com>
2017-07-19 12:27:38 +0800

    细节修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 //index.js
+var util = require('../../utils/util');
 //获取应用实例
 var app = getApp();
 var __obj = [];
@@ -44,22 +45,18 @@ Page({
                                 id : wx.getStorageSync('user').user_id
                             },
                             success: function (res) {
-                                var books = res.data;
                                 if(res.data){
-                                    var _data = [];
-                                    for(var i =books.length-1 ;i>books.length-4;i--){
-                                        var _obj = {};
-                                        _obj = books[i];
-                                        _obj.created_at = app.getDateDiff(books[i].created_at);
-                                        _data.push(_obj);
-                                    }
-                                    __obj = __obj.concat(_data);
+                                    var books = res.data;
+                                    books.forEach(function (p1, p2, p3) {
+                                        p1.created_at =app.getDateDiff(p1.created_at);
+                                    });
+                                    __obj = __obj.concat(books);
                                     //调用应用实例的方法获取全局数据
                                     app.getUserInfo(function(userInfo){
                                         user_info = userInfo;
                                     });
                                     that.setData({
-                                        obj: __obj,
+                                        obj: __obj.sort(util.compare('updated_at')),
                                         userInfo:user_info
                                     })
                                 }
@@ -69,40 +66,17 @@ Page({
                 }
             }
         });
-
     },
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-        __obj = [];
+        var __obj = [];
+        wx.showLoading({
+            title: '页面加载中...',
+            mask: true
+        });
         var that = this;
-        //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo){
-            //更新数据
-            that.setData({
-                userInfo:userInfo
-            });
-        });
-        wx.request({
-            url: 'https://johnnyzhang.cn/wxxcx/get/book',
-            data: {
-                id : wx.getStorageSync('user').user_id
-            },
-            success: function (res) {
-                var books = res.data;
-                if(res.data){
-                    var _data = [];
-                    for(var i =books.length-1 ;i>books.length-4;i--){
-                        var _obj = {};
-                        _obj = books[i];
-                        _obj.created_at = app.getDateDiff(books[i].created_at);
-                        _data.push(_obj);
-                    }
-                    __obj = __obj.concat(_data);
-                }
-            }
-        });
         wx.request({
             url: 'https://johnnyzhang.cn/wxxcx/get/posts',
             data: {
@@ -113,6 +87,7 @@ Page({
                     var posts = resp.data;
                     if(resp.data){
                         var _data = [];
+                        var user_info = null;
                         for(var i =posts.length-1 ;i>=0;i--){
                             var _obj = {};
                             _obj = posts[i];
@@ -120,11 +95,31 @@ Page({
                             _data.push(_obj);
                         }
                         __obj = __obj.concat(_data);
-                        that.setData({
-                            obj: __obj
+                        wx.request({
+                            url: 'https://johnnyzhang.cn/wxxcx/get/book',
+                            data: {
+                                id : wx.getStorageSync('user').user_id
+                            },
+                            success: function (res) {
+                                if(res.data){
+                                    var books = res.data;
+                                    books.forEach(function (p1, p2, p3) {
+                                        p1.created_at =app.getDateDiff(p1.created_at);
+                                    });
+                                    __obj = __obj.concat(books);
+                                    //调用应用实例的方法获取全局数据
+                                    app.getUserInfo(function(userInfo){
+                                        user_info = userInfo;
+                                    });
+                                    that.setData({
+                                        obj: __obj.sort(util.compare('updated_at')),
+                                        userInfo:user_info
+                                    });
+                                    wx.hideLoading();
+                                    wx.stopPullDownRefresh();
+                                }
+                            }
                         });
-                        wx.hideLoading();
-                        wx.stopPullDownRefresh();
                     }
                 }
             }
--- utils/util.js
+++ utils/util.js
@@ -14,7 +14,14 @@ function formatNumber(n) {
   n = n.toString();
   return n[1] ? n : '0' + n
 }
-
+function compare(property){
+    return function(a,b){
+        var value1 = a[property];
+        var value2 = b[property];
+        return value1 - value2;
+    }
+}
 module.exports = {
-  formatTime: formatTime
+    formatTime: formatTime,
+    compare:compare
 };
